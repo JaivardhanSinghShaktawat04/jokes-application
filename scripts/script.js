4a699673b391c7151a60f9fd2ed6a2b0be40e3c5
JaivardhanSinghShaktawat04 <shaktawatjaivardhansingh.tech04@gmail.com>
2023-05-07 23:04:49 +0530

    Next Joke Functionality Added

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,28 @@
-console.log('Hello World');
+const btnNextJoke = document.querySelector('.btn-next_joke');
+const setupElm = document.querySelector('.setup');
+const deliveryElm = document.querySelector('.delivery');
+const api = `https://v2.jokeapi.dev/joke/Any?safe-mode` ; 
+
+const showNextJoke = async () => {
+    try{
+      const response = await window.fetch(api);
+      console.log('i am working');
+      const {setup , delivery , joke} = await response.json();
+      if(joke){
+         setupElm.style.display = "none" ; 
+         deliveryElm.innerHTML = joke ; 
+      }else{
+        setupElm.style.display === "none" ? setupElm.style.display = "block" : "" ; 
+        setupElm.innerHTML = setup ; 
+        deliveryElm.innerHTML = delivery ; 
+      }
+    }catch(err){
+
+    }
+}
+
+const initApplication = () => showNextJoke();
+
+initApplication();
+
+btnNextJoke.addEventListener('click' , showNextJoke);
