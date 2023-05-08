8f8bdb16494fd529e2045442831bc82a61480068
JaivardhanSinghShaktawat04 <shaktawatjaivardhansingh.tech04@gmail.com>
2023-05-08 08:59:36 +0530

    About Creator Functionality Added

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,15 @@
 const btnNextJoke = document.querySelector('.btn-next_joke');
 const setupElm = document.querySelector('.setup');
 const deliveryElm = document.querySelector('.delivery');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnAboutCreator = document.querySelector('.btn-about_creator');
+const modalWindowElm = document.querySelector('.modal-window');
+
 const api = `https://v2.jokeapi.dev/joke/Any?safe-mode` ; 
 
 const showNextJoke = async () => {
+  console.log(btnNextJoke);
     try{
       const response = await window.fetch(api);
       console.log('i am working');
@@ -17,12 +23,21 @@ const showNextJoke = async () => {
         deliveryElm.innerHTML = delivery ; 
       }
     }catch(err){
-
+        throw err ; 
     }
+}
+
+const toggleModalWindow = () => {
+  overlay.classList.toggle('hidden');
+  modalWindowElm.classList.toggle('hidden');
 }
 
 const initApplication = () => showNextJoke();
 
 initApplication();
 
+
+btnAboutCreator.addEventListener('click' , toggleModalWindow);
+overlay.addEventListener('click' , toggleModalWindow);
+btnCloseModal.addEventListener('click' , toggleModalWindow);
 btnNextJoke.addEventListener('click' , showNextJoke);
